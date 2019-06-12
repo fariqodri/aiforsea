4e77160a735a6c689ea403a3fd29aeb999564a11
fari.qodri <arfixfari@gmail.com>
2019-06-12 11:43:09 +0700

    finish with 75% accuracy

---
+++ dangerous_driving.py
@@ -0,0 +1,11 @@
+from detection import Detection
+
+
+class DangerousDrivingDetection(Detection):
+    def split_feature_and_label(self, label_column):
+        x = self.data.drop(label_column, axis=1)
+        y = self.data[label_column]
+        return x, y
+
+    def merge_label_to_data(self, label_df, on):
+        self.data = self.data.merge(label_df, on=on)
